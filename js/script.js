326381df67d107c004260206681e52b4aeac6bef
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-03-17 20:46:17 +0200

    scroll reveal

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 }
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '85px',
+    duration: 2500,
+    reset: true
+})
+
+sr.reveal ('.home-text', {delay:300});
+sr.reveal('.home-img', {delay:400});
+sr.reveal('.container', {delay:400});
+
+sr.reveal('.about-img', {});
+sr.reveal('.about-text', {delay: 300});
+
+sr.reveal('.middle-text', {});
+sr.reveal('.row-btn,.shop-content', {delay: 300});
+
+sr.reveal('.review-content,.contact', {delay: 300});
